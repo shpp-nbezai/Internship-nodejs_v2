d74752c0e753039ce3c812a59c62443bca19fbca
Nikolay Bezay <niko@adoric.com>
2021-06-17 03:31:53 +0300

    functions done

--- week1/function.js
+++ week1/function.js
@@ -3,9 +3,9 @@
 function mySum() {
     const args = [...arguments];
     let result = 0;
+
     args.forEach(item => {
         result += item;
-        console.log(item);
     });
     return result;
 }
@@ -13,37 +13,46 @@ function mySum() {
 const mySum2 = function () {
     const args = [...arguments];
     let result = 0;
+
     args.forEach(item => {
         result += item;
-        console.log(item);
     });
     return result;
 };
 
-mySum(1, 2);
-mySum2(1, 2);
+console.log(mySum(1, 2, 3, 4, 5));
+console.log(mySum2(3, 4, 5, 6, 7));
 // 1. Create self-invoked anonymous function, that will return if the day after tomorrow is odd, or even.
 // HINT: function accepts one argument: new Date(); 
 // output example 15.06.2021, 11:19:22 is odd date
+(function(date = new Date()) {
+    const dayAfterTommorow = new Date(date);
 
-/*
+    dayAfterTommorow.setDate(dayAfterTommorow.getDate() + 2);
+    let dayType = dayAfterTommorow.getDay % 2 !== 0 ? 'odd' : 'even';
+
+    console.log(`${dayAfterTommorow.toLocaleDateString()}, ${dayAfterTommorow.toLocaleTimeString()} is ${dayType} date`);
+})();
+
 // 2. Rewrite this function to ES6/ES2015
 
-var Intern = function(name, age) {
-    this.name = name;
-    this.age = age;
-    this.homework = {
-        status: 'Resolved',
-        rating: '100%'
+class Intern {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.homework = {
+            status: 'Resolved',
+            rating: '100%'
+        }
+    }
+    getHomework() {
+        return `${this.name} resolved homework with status ${this.homework.status}`;
     }
 }
 
-Intern.prototype.getHomework = function () {
-    return `${this.name} resolved homework with status ${this.homework.status}`;
-}
+const testIntern = new Intern('niko', '33');
 
-const ***** = new Intern(****, ***);
-
+console.log(testIntern.getHomework());
 
 // 3. Write multiply function. Two arguments - random numbers. But sometimes, we don't have second argument, and result is = random number * 12
 // HINT: Read about default parameters in functions.
@@ -53,9 +62,12 @@ const ***** = new Intern(****, ***);
  * (19, 29) => 551
  * (23) => 276
  */
+const myMultiply = (number1, number2 = 12) => {
+    return number1 * number2;
+}
 
-
-
+console.log(myMultiply(19, 29));
+console.log(myMultiply(23));
 /**
  * Write function, that modify first argument to example output with delay 2 seconds
  * 
@@ -65,5 +77,11 @@ const ***** = new Intern(****, ***);
  */
 
 function yourFuncName(options, callback) {
-    // your code here
+    for(key in options) {
+        options[key]++;
+    }
+    setTimeout(function() {
+        callback(options);
+    }, 2000);
 }
+yourFuncName({ a: 1, b: 2}, (data) => { console.log(data) });
